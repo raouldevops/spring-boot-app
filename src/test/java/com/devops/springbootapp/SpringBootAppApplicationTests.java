895c09cf647bc28f269d73409d64a2505709c73e
vianick <cipahraoul2016@gmail.com>
2024-05-28 20:52:08 -0400

    fix test

--- src/test/java/com/devops/springbootapp/SpringBootAppApplicationTests.java
+++ src/test/java/com/devops/springbootapp/SpringBootAppApplicationTests.java
@@ -1,27 +1,11 @@
 package com.devops.springbootapp;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootAppApplicationTests {
-
-	private SpringBootAppApplication springBootAppApplication;
-
-	public SpringBootAppApplicationTests(SpringBootAppApplication springBootAppApplication) {
-		this.springBootAppApplication = springBootAppApplication;
-	}
-
 	@Test
 	void contextLoads() {
 	}
-
-	@Test
-	void testHome() {
-		String actual = springBootAppApplication.Home();
-		Assertions.assertEquals("index", actual);//NOSONAR
-	}
-
 }
