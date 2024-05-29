d01d11262363999cfd9f178be74ebbb6b67887e7
vianick <cipahraoul2016@gmail.com>
2024-05-28 20:14:32 -0400

    jenkins cicd

--- src/test/java/com/devops/springbootapp/SpringBootAppApplicationTests.java
+++ src/test/java/com/devops/springbootapp/SpringBootAppApplicationTests.java
@@ -1,13 +1,27 @@
 package com.devops.springbootapp;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootAppApplicationTests {
-
+	@Autowired
+	SpringBootAppApplication springBootAppApplication;
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testHello() {
+		System.out.println("Hello ceci est un test unitaire 1");
+	}
+
+	@Test
+	void testHome() {
+		String actual = springBootAppApplication.Home();
+		Assertions.assertEquals("index", actual);
 	}
 
 }
